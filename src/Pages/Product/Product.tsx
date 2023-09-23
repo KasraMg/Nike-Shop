033dee2fd2552b76dc92a2ac3ecd38fb723ce8ef
KasraMg <kasragithub7@gmail.com>
2023-09-23 13:24:54 +0330

    productPageResponsive

--- src/Pages/Product/Product.tsx
+++ src/Pages/Product/Product.tsx
@@ -23,12 +23,12 @@ const Product = () => {
     }, [])
 
     return (
-        <div style={{ boxShadow: ' 0px 15px 50px 10px rgba(0, 0, 0, 0.4' }} className="card mt-12 bg-[#024742] items-center h-full w-full m-auto  productPage">
+        <div style={{ boxShadow: ' 0px 15px 50px 10px rgba(0, 0, 0, 0.4' }} className="card md:mt-0 mt-12 bg-[#024742] items-center h-full w-full m-auto md:h-[unset] productPage">
 
-            <main className=' relative mx-auto w-max pt-28 mt-8  z-[9999]'>
+            <main className=' relative mx-auto md:mt-0 md:pt-20 w-max pt-28 mt-8 md:w-full  z-[9999]'>
                 {productInfo && (
                     <>
-                        <div style={{ background: `linear-gradient(135deg, ${productInfo.bg} 8%, #2a6e6a 83%)`, content: '', borderRadius: ' 0% 50% 50% 0%' }} className="h-[500px] w-[400px] flex items-center absolute z-[5] bg-[#ff6d39] mr-[93px]">
+                        <div style={{ background: `linear-gradient(135deg, ${productInfo.bg} 8%, #2a6e6a 83%)`, content: '', borderRadius: ' 0% 50% 50% 0%' }} className="h-[500px] sm:h-[300px] md:w-full md:!rounded-none md:relative w-[400px] flex items-center absolute z-[5] bg-[#ff6d39] mr-[93px]">
 
                             <Swiper
                                 pagination={{
@@ -36,26 +36,26 @@ const Product = () => {
                                 }}
                                 rewind={true}
                                 modules={[Pagination]}
-                                className="mySwiper w-[500px] absolute bottom-24"
+                                className="mySwiper sm:pb-4 md:!w-full lg:w-[416px] w-[500px] md:relative md:bottom-0 absolute bottom-24"
                             >
-                                <SwiperSlide className='!w-[105%]'>  <img src={productInfo.image} className='-ml-[65px] w-[576px] mt-[60px]' alt="shoe" /></SwiperSlide>
-                                <SwiperSlide className='!w-[105%]'>  <img src={productInfo.image2} className='-ml-[65px] w-[576px] mt-[60px]' alt="shoe" /></SwiperSlide>
-                                <SwiperSlide className='!w-[105%]'>  <img src={productInfo.image3} className='-ml-[65px] w-[576px] mt-[60px]' alt="shoe" /></SwiperSlide>
+                                <SwiperSlide className=' product-Slide  lg:pl-3'>  <img src={productInfo.image} className='-ml-[65px] w-[576px]  lg:w-[490px] md:!w-[100%]     md:mx-auto mt-[60px] md:mt-0' alt="shoe" /></SwiperSlide>
+                                <SwiperSlide className=' product-Slide  lg:pl-3'>  <img src={productInfo.image2} className='-ml-[65px] w-[576px]  lg:w-[490px] md:!w-[100%]     md:mx-auto mt-[60px] md:mt-0' alt="shoe" /></SwiperSlide>
+                                <SwiperSlide className=' product-Slide  lg:pl-3'>  <img src={productInfo.image3} className='-ml-[65px] w-[576px]  lg:w-[490px] md:!w-[100%]     md:mx-auto mt-[60px] md:mt-0' alt="shoe" /></SwiperSlide>
 
                             </Swiper>
                         </div>
-                        <div style={{ content: '' }} className="h-[500px] relative bg-[#ffffff] w-[800px] z-[3] ml-[200px] content">
-                            <div className=" absolute top-1 right-[64px] h-[500px] w-[400px] z-10">
+                        <div style={{ content: '' }} className="h-[500px] relative sm-x2:!h-full bg-[#ffffff] lg:w-[700px] md:!w-full md:ml-0 w-[800px] z-[3] ml-[200px] content">
+                            <div className=" absolute md:relative sm-x2:!h-full sm-x2:pb-10 md:right-0 sm-x2:!px-4 md:px-8 md:w-full top-1 right-[64px] h-[500px]   w-[400px] z-10">
                                 <div className="flex justify-between pt-5">
                                     <h1 className='text-[#024742] font-[cursive] text-3xl'>Nike</h1>
-                                    <SiJordan className='text-[#024742] relative left-8 text-2xl cursor-pointer' />
+                                    <SiJordan className='text-[#024742] relative md:left-0 left-8 text-2xl cursor-pointer' />
                                 </div>
 
                                 <div className=" font-[cursive] uppercase ">
                                     <h3 className='mt-16'>disiend by {productInfo.Designed_by}</h3>
-                                    <p className='my-4 text-3xl whitespace-nowrap'>{productInfo.title}</p>
+                                    <p className='my-4 sm-x2:whitespace-break-spaces text-3xl whitespace-nowrap'>{productInfo.title}</p>
 
-                                    <section className='flex justify-between'>
+                                    <section className='flex justify-between sm-x2:flex-col sm-x2:gap-5'>
 
                                         <div className='flex gap-5'>
                                             <h4> ${productInfo.price.current_price}</h4>
@@ -77,7 +77,7 @@ const Product = () => {
                                     </section>
 
                                 </div>
-                                <div className='flex gap-9 justify-between my-3'>
+                                <div className='flex sm-x2:mt-5  gap-9 justify-between my-3'>
                                     <p>SIZE</p>
                                     <ul className='flex gap-2'>
                                         {productInfo.size.map(data => (
